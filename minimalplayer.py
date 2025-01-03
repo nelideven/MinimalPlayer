2b0d0d127e64b50c9c8f7487611350af9b93c8f4
nelideven <nnyomandhipa@outlook.com>
2025-01-03 23:11:03 +0800

    Update minimalplayer.py - v1.2.1

--- minimalplayer.py
+++ minimalplayer.py
@@ -6,7 +6,7 @@
 #*
 #* You should have received a copy of the GNU General Public License (LICENSE) along with this project. If not, see <https://www.gnu.org/licenses/>.
 #*
-#* You should also have a copy of the mp_autocomplete (for Linux and macOS) or mp_autocomplete.ps1 (for Windows) script, which is required for the file name input in this software (if you downloaded the source code). 
+#* You should also have a copy of the mp_autocomplete (for Linux) or mp_autocomplete.ps1 (for Windows) script, which is required for the file name input in this software (if you downloaded the source code). 
 
 import subprocess
 import os
@@ -65,7 +65,7 @@ def calculate_sample_rate(speed_factor, pipe_path=None):
     else:
         return int(original_rate * speed_factor)
 
-def play_audio(player, file_path, new_sample_rate, pipe_path=None, ffmpeg_exist=True, out_format='s16le', fileargs=""):
+def play_audio(player, file_path, new_sample_rate, pipe_path=None, ffmpeg_exist=True, out_format='s16le', nogui=False, fileargs=""):
     if pipe_path:
         if ffmpeg_exist:
             print(f'Piping converted file {file_path} with format {out_format} to {pipe_path}. Press q to exit')
@@ -98,13 +98,20 @@ def play_audio(player, file_path, new_sample_rate, pipe_path=None, ffmpeg_exist=
             elif player == "ffplay":
                 if os.name == "nt":
                     if fileargs == None:
-                        print(fileargs)
-                        command = f'ffmpeg -i {file_path} -ar 44100 -f wav -loglevel warning -stats - | ffplay -ar {new_sample_rate} -loglevel warning -autoexit -'
+                        if nogui:
+                            command = f'ffmpeg -i {file_path} -ar 44100 -f wav -loglevel warning -stats - | ffplay -ar {new_sample_rate} -loglevel warning -autoexit -nodisp -'
+                        else:
+                            command = f'ffmpeg -i {file_path} -ar 44100 -f wav -loglevel warning -stats - | ffplay -ar {new_sample_rate} -loglevel warning -autoexit -'
                     else:
-                        print(fileargs)
-                        command = f'ffmpeg -i "{file_path}" -ar 44100 -f wav -loglevel warning -stats - | ffplay -ar {new_sample_rate} -loglevel warning -autoexit -'
+                        if nogui:
+                            command = f'ffmpeg -i "{file_path}" -ar 44100 -f wav -loglevel warning -stats - | ffplay -ar {new_sample_rate} -loglevel warning -autoexit -nodisp -'
+                        else:
+                            command = f'ffmpeg -i "{file_path}" -ar 44100 -f wav -loglevel warning -stats - | ffplay -ar {new_sample_rate} -loglevel warning -autoexit -'
                 else:
-                    command = f'ffmpeg -i "{file_path}" -ar 44100 -f wav -loglevel warning -stats - | ffplay -ar {new_sample_rate} -loglevel warning -autoexit -'
+                    if nogui:
+                        command = f'ffmpeg -i "{file_path}" -ar 44100 -f wav -loglevel warning -stats - | ffplay -ar {new_sample_rate} -loglevel warning -autoexit -nodisp -'
+                    else:
+                        command = f'ffmpeg -i "{file_path}" -ar 44100 -f wav -loglevel warning -stats - | ffplay -ar {new_sample_rate} -loglevel warning -autoexit -'
         else:
             print(f'Playing audio file {file_path} without speed modifications. Press CTRL+C to exit')
             if player == "paplay":
@@ -119,13 +126,16 @@ def main():
     parser.add_argument('-f', '--file', type=str, help='Audio file path')
     parser.add_argument('-s', '--speed', type=float, default=0.0, help='Speed factor (e.g., 0.5 for half speed, 2.0 for double speed)')
     parser.add_argument('-o', '--output', type=str, help='Output file format (only for pipe mode)')
-    parser.add_argument('--extm', action="store_true", help='You might not want to mess with this...')
+    parser.add_argument('-pl', '--player', type=str, help='State which player to use. Only for Linux')
+    parser.add_argument('-nui', '--nogui', action="store_true", help='No-GUI mode for FFplay. This disables the spectogram GUI.')
+    parser.add_argument('--extm', action="store_true")
 
     args = parser.parse_args()
     pipe_mode = args.pipe
     file_path = args.file
     speed_factor = args.speed
     out_format = args.output
+    player = args.player
     
     if args.output and not args.pipe:
         parser.error("-o/--output requires -p/--pipe to be specified")
@@ -137,27 +147,66 @@ def main():
         ffmpeg_exist = False
     
     # Determine which player to use
-    player = ""
-    if ffmpeg_exist:
-        if os.name == "posix":
-            if check_binary("pacat") & check_binary("pw-cat"):
-                if pipe_mode:
-                    player = None
-                elif args.extm:
+    if player == None:
+        if ffmpeg_exist:
+            if os.name == "posix":
+                if check_binary("pacat") & check_binary("pw-cat"):
+                    if pipe_mode:
+                        player = None
+                    elif args.extm:
+                        if check_binary("ffplay"):
+                            player = "ffplay"
+                        else:
+                            player = "pw-cat"
+                    else:
+                        player = input("You have pw-cat and pacat. Choose one to use (or choose others (ffplay, paplay, pw-play)): ").strip()
+                elif check_binary("pw-cat"):
                     player = "pw-cat"
+                elif check_binary("pacat"):
+                    player = "pacat"
                 else:
-                    player = input("You have pw-cat and pacat. Choose one to use (or choose others (ffplay, paplay, pw-play)): ").strip()
-            elif check_binary("pw-cat"):
-                player = "pw-cat"
-            elif check_binary("pacat"):
-                player = "pacat"
+                    print("Neither pacat nor pw-cat detected! Falling back to paplay/pw-play")
+                    if check_binary("paplay") & check_binary("pw-play"):
+                        if pipe_mode:
+                            player = None
+                        elif args.extm:
+                            if check_binary("ffplay"):
+                                player = "ffplay"
+                            else:
+                                player = "pw-play"
+                        else:
+                            player = input("You have pw-play and paplay. Choose one to use: ").strip()
+                    elif check_binary("pw-play"):
+                        player = "pw-play"
+                    elif check_binary("paplay"):
+                        player = "paplay"
+                    else:
+                        print("Neither pacat, paplay, pw-cat, nor pw-play detected! Falling back to FFplay")
+                        if check_binary("ffplay"):
+                            player = "ffplay"
+                        else:
+                            print("Neither pacat, paplay, pw-cat, pw-play, nor FFplay detected! Exiting..")
+                            time.sleep(1)
+                            exit(127)
             else:
-                print("Neither pacat nor pw-cat detected! Falling back to paplay/pw-play")
+                if check_binary("ffplay"):
+                    player = "ffplay"
+                else:
+                    print("FFplay not detected! Make sure FFplay is located in the environment variables. Exiting..")
+                    time.sleep(1)
+                    exit(127)
+    
+        else:
+            if os.name == "posix":
+                print("FFmpeg does not exist! Audio file would be played without modifications (now relies on paplay/pw-play)")
                 if check_binary("paplay") & check_binary("pw-play"):
                     if pipe_mode:
                         player = None
                     elif args.extm:
-                        player = "pw-play"
+                        if check_binary("ffplay"):
+                            player = "ffplay"
+                        else:
+                            player = "pw-play"
                     else:
                         player = input("You have pw-play and paplay. Choose one to use: ").strip()
                 elif check_binary("pw-play"):
@@ -165,44 +214,92 @@ def main():
                 elif check_binary("paplay"):
                     player = "paplay"
                 else:
-                    print("Neither pacat, paplay, pw-cat, nor pw-play detected! Falling back to FFplay")
+                    print("Neither paplay nor pw-play detected! Exiting..")
+                    time.sleep(1)
+                    exit(127)
+            else:
+                print("FFmpeg does not exist! Generally FFplay relies on FFmpeg, so exiting..")
+                time.sleep(1)
+                exit(127)
+    else:
+        if check_binary(player):
+            pass
+        else:
+            print(f"{player} does not exist!")
+            if ffmpeg_exist:
+                if os.name == "posix":
+                    if check_binary("pacat") & check_binary("pw-cat"):
+                        if pipe_mode:
+                            player = None
+                        elif args.extm:
+                            if check_binary("ffplay"):
+                                player = "ffplay"
+                            else:
+                                player = "pw-cat"
+                        else:
+                            player = input("You have pw-cat and pacat. Choose one to use (or choose others (ffplay, paplay, pw-play)): ").strip()
+                    elif check_binary("pw-cat"):
+                        player = "pw-cat"
+                    elif check_binary("pacat"):
+                        player = "pacat"
+                    else:
+                        print("Neither pacat nor pw-cat detected! Falling back to paplay/pw-play")
+                        if check_binary("paplay") & check_binary("pw-play"):
+                            if pipe_mode:
+                                player = None
+                            elif args.extm:
+                                if check_binary("ffplay"):
+                                    player = "ffplay"
+                                else:
+                                    player = "pw-play"
+                            else:
+                                player = input("You have pw-play and paplay. Choose one to use: ").strip()
+                        elif check_binary("pw-play"):
+                            player = "pw-play"
+                        elif check_binary("paplay"):
+                            player = "paplay"
+                        else:
+                            print("Neither pacat, paplay, pw-cat, nor pw-play detected! Falling back to FFplay")
+                            if check_binary("ffplay"):
+                                player = "ffplay"
+                            else:
+                                print("Neither pacat, paplay, pw-cat, pw-play, nor FFplay detected! Exiting..")
+                                time.sleep(1)
+                                exit(127)
+                else:
                     if check_binary("ffplay"):
                         player = "ffplay"
                     else:
-                        print("Neither pacat, paplay, pw-cat, pw-play, nor FFplay detected! Exiting..")
+                        print("FFplay not detected! Make sure FFplay is located in the environment variables. Exiting..")
                         time.sleep(1)
                         exit(127)
-        else:
-            if check_binary("ffplay"):
-                player = "ffplay"
+        
             else:
-                print("FFplay not detected! Make sure FFplay is located in the environment variables. Exiting..")
-                time.sleep(1)
-                exit(127)
-
-    else:
-        if os.name == "posix":
-            print("FFmpeg does not exist! Audio file would be played without modifications (now relies on paplay/pw-play)")
-            if check_binary("paplay") & check_binary("pw-play"):
-                if pipe_mode:
-                    player = None
-                elif args.extm:
-                    player = "pw-play"
+                if os.name == "posix":
+                    print("FFmpeg does not exist! Audio file would be played without modifications (now relies on paplay/pw-play)")
+                    if check_binary("paplay") & check_binary("pw-play"):
+                        if pipe_mode:
+                            player = None
+                        elif args.extm:
+                            if check_binary("ffplay"):
+                                player = "ffplay"
+                            else:
+                                player = "pw-play"
+                        else:
+                            player = input("You have pw-play and paplay. Choose one to use: ").strip()
+                    elif check_binary("pw-play"):
+                        player = "pw-play"
+                    elif check_binary("paplay"):
+                        player = "paplay"
+                    else:
+                        print("Neither paplay nor pw-play detected! Exiting..")
+                        time.sleep(1)
+                        exit(127)
                 else:
-                    player = input("You have pw-play and paplay. Choose one to use: ").strip()
-            elif check_binary("pw-play"):
-                player = "pw-play"
-            elif check_binary("paplay"):
-                player = "paplay"
-            else:
-                print("Neither paplay nor pw-play detected! Exiting..")
-                time.sleep(1)
-                exit(127)
-        else:
-            print("FFmpeg does not exist! Generally FFplay relies on FFmpeg, so exiting..")
-            time.sleep(1)
-            exit(127)
-    
+                    print("FFmpeg does not exist! Generally FFplay relies on FFmpeg, so exiting..")
+                    time.sleep(1)
+                    exit(127)
+                
     # Handle pipe mode and arguments
     if pipe_mode:
         if pipe_mode == True:
@@ -248,7 +345,7 @@ def main():
 
     new_sample_rate = calculate_sample_rate(speed_factor, pipe_path if pipe_mode else None)
     print(f'Sample rate: {new_sample_rate}')
-    play_audio(player, file_path, new_sample_rate, pipe_path if pipe_mode else None, ffmpeg_exist, out_format, args.file)
+    play_audio(player, file_path, new_sample_rate, pipe_path if pipe_mode else None, ffmpeg_exist, out_format, args.nogui, args.file)
 
 if __name__ == '__main__':
     try:
@@ -258,3 +355,6 @@ if __name__ == '__main__':
         time.sleep(10)
         print("Exiting..")
         time.sleep(0.1)
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt caught! Exiting..")
+        time.sleep(0.5)
